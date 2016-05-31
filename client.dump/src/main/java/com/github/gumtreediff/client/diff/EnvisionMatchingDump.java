94ec265f82e0d039b0707a27dca9ed013c7a6dc3
Balz Guenat <balz.guenat@gmail.com>
2016-05-31 15:06:51 +0200

    sort patches and write to file

--- client.dump/src/main/java/com/github/gumtreediff/client/diff/EnvisionMatchingDump.java
+++ client.dump/src/main/java/com/github/gumtreediff/client/diff/EnvisionMatchingDump.java
@@ -1,11 +1,16 @@
 package com.github.gumtreediff.client.diff;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 import com.github.gumtreediff.client.Register;
-import com.github.gumtreediff.matchers.MappingStore;
+import com.github.gumtreediff.matchers.Mapping;
 import com.github.gumtreediff.matchers.Matcher;
-import com.github.gumtreediff.tree.ITree;
-import com.github.gumtreediff.tree.TreeContext;
-import com.github.gumtreediff.tree.TreeUtils;
 
 @Register(name = "envdmp", description = "Dump line matching",
         experimental = true, options = AbstractDiffClient.Options.class)
@@ -14,6 +19,8 @@ public class EnvisionMatchingDump extends AbstractDiffClient<AbstractDiffClient.
 	private static final String LINE_NUM = "lineNum";
 	private static final String NODE_ID = "nodeId";
 	private static final String PARENT_ID = "parentId";
+	
+	private static final String OUTPUT_FILE_SUFFIX = ".idpatch";
 	
     public EnvisionMatchingDump(String[] args) {
         super(args);
@@ -26,22 +33,32 @@ public class EnvisionMatchingDump extends AbstractDiffClient<AbstractDiffClient.
 
     @Override
     public void run() {
-        Matcher m = matchTrees();
-        final MappingStore map = m.getMappings();
-        TreeContext ctx = getDstTreeContext();
-    	TreeUtils.visitTree(ctx.getRoot(), new TreeUtils.TreeVisitor() {
-            public void startTree(ITree tree) {
-            	ITree match = map.getSrc(tree);
-            	if (match != null)
-            	{
-            		String outputLine = String.format("%d -> %s, %s", 
-            				tree.getMetadata(LINE_NUM), 
-            				match.getMetadata(NODE_ID), 
-            				match.getMetadata(PARENT_ID) );
-            		System.out.println(outputLine);
-            	}
-            }
-            public void endTree(ITree tree) {}
-        });
+        Matcher matcher = matchTrees();
+        ArrayList<Mapping> mappings = new ArrayList<>(matcher.getMappingSet());
+        mappings.sort(new Comparator<Mapping>() {
+
+			@Override
+			public int compare(Mapping a, Mapping b) {
+				return (Integer) a.getSecond().getMetadata(LINE_NUM) - (Integer) b.getSecond().getMetadata(LINE_NUM);
+			}
+		});
+        
+		try {
+	        File dstFile = new File(opts.dst);
+	        File outputFile = new File(dstFile.getParentFile(), dstFile.getName() + OUTPUT_FILE_SUFFIX);
+			PrintWriter writer = new PrintWriter(outputFile);
+	        for (Mapping m : mappings) {
+	    		String outputLine = String.format("%d, %s, %s", 
+	    				m.getSecond().getMetadata(LINE_NUM), 
+	    				m.getFirst().getMetadata(NODE_ID), 
+	    				m.getFirst().getMetadata(PARENT_ID) );
+	    		
+	    		writer.println(outputLine);
+	        }
+	        writer.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not write patch file.");
+			e.printStackTrace();
+		}
     }
 }
