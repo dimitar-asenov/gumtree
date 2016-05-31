d26ee52319ae2bfb58aa480a0b2095177adbf6ee
Balz Guenat <balz.guenat@gmail.com>
2016-05-31 14:39:20 +0200

    envision tree generator package setup

---
+++ gen.envision/src/main/java/gen/envision/EnvisionTreeGenerator.java
@@ -0,0 +1,18 @@
+
+package gen.envision;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import com.github.gumtreediff.gen.Register;
+import com.github.gumtreediff.gen.TreeGenerator;
+import com.github.gumtreediff.tree.TreeContext;
+
+@Register(id = "envision", accept = "\\$")
+public class EnvisionTreeGenerator extends TreeGenerator {
+	@Override
+	protected TreeContext generate(Reader r) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
